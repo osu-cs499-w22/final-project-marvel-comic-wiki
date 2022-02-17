d5810164ed138e4021b3538532c012224672a75f
Jaiden Hodson <55996217+PvtGandalf@users.noreply.github.com>
2022-02-17 13:26:52 -0800

    API Data Retrieved
    - API Data Retrieval Implemented
    - API Keys moved into .env variables
    - Updated README (API Key info)

--- src/pages/characters.js
+++ src/pages/characters.js
@@ -1,6 +1,7 @@
 import Header from '../components/header';
 import Footer from '../components/footer';
 import styled from '@emotion/styled/macro';
+import useMarvelSearch from '../hooks/useMarvelSearch';
 
 const Title = styled.h1`
   text-align: center;
@@ -8,10 +9,19 @@ const Title = styled.h1`
 `;
 
 function Characters() {
+
+  const url = `https://gateway.marvel.com/v1/public/characters`;
+  const [ characters, loading, error ] = useMarvelSearch(url);
+  
   return (
     <div>
 	  <Header></Header>
       <Title>Characters</Title>
+      <ul>
+          {characters.map(character => (
+            <li>{character.name}</li>
+          ))}
+        </ul>
       <Footer></Footer>
     </div>
   );
