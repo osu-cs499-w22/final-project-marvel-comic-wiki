cca00276916b79067277a7c31ce3ad625079f801
Jaiden Hodson <55996217+PvtGandalf@users.noreply.github.com>
2022-02-17 19:22:02 -0800

    Character Cards Implemented
    - Each character has their own card
    - Each card has a name and image
    - Footer is positioned to bottom of page

--- src/pages/characters.js
+++ src/pages/characters.js
@@ -4,6 +4,7 @@ import styled from '@emotion/styled/macro';
 import useMarvelSearch from '../hooks/useMarvelSearch';
 import Spinner from '../components/spinner';
 import ErrorContainer from '../components/errorContainer';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 const Title = styled.h1`
   text-align: center;
@@ -15,24 +16,54 @@ const Loading = styled.div`
   padding-top: 20px;
 `;
 
+const StyledContainer = styled(Container)`
+  max-width: 95%;
+`;
+
+const StyledCardBody = styled(Card.Body)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 100px;
+`;
+
+const StyledCardTitle = styled(Card.Title)`
+  text-align: center;
+  padding-bottom: 5px;
+`;
+
+const StyledCardText = styled(Card.Text)`
+  text-align: center;
+  margin-bottom: 0px;
+`;
+
+
 function Characters() {
 
   const url = `https://gateway.marvel.com/v1/public/characters`;
-  const [ characters, loading, error ] = useMarvelSearch(url);
+  const [ characters, loadingAll, errorAll ] = useMarvelSearch(url);
   
   return (
     <div>
 	  <Header></Header>
       <Title>Characters</Title>
-      {console.log("loading: ", loading)}
-      {loading ? ( <Loading> <Spinner /> </Loading> ) : (
-        <ul>
-          {characters.map(character => (
-            <li key={character.id}>{character.name}</li>
-          ))}
-        </ul>
+      {loadingAll ? ( <Loading> <Spinner /> </Loading> ) : (
+        <StyledContainer>
+          <Row className="row-cols-1 row-cols-md-4 g-4">
+            {characters.map(character =>
+              <Col key={character.id}>
+                <Card>
+                  <img src={`${character.thumbnail.path}/standard_xlarge.${character.thumbnail.extension}`}className="card-img-top"alt=""></img>
+                  <StyledCardBody>
+                    <StyledCardTitle>{character.name}</StyledCardTitle>
+                  </StyledCardBody>
+                </Card>
+              </Col>
+            )}
+          </Row>
+        </StyledContainer>
       )}
-      {error && <ErrorContainer>Error!</ErrorContainer>}
+      {errorAll && <ErrorContainer>Error!</ErrorContainer>}
       <Footer></Footer>
     </div>
   );
