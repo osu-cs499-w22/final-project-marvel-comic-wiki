aa06b59cc1748e08050b88750d98662bcc1d3934
Jaiden Hodson <55996217+PvtGandalf@users.noreply.github.com>
2022-03-13 14:22:23 -0700

    Removed xxl class
    - react-bootstrap doesn't have a 7 col size

--- src/pages/characters.js
+++ src/pages/characters.js
@@ -143,7 +143,7 @@ function Characters() {
             <StyledInput placeholder= 'Enter a character name ' onChange={e => setCharacterToSearch(e.target.value)} /> 
           </StyledForm>
         
-          <Row className="row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-xl-6 row-cols-xxl-7 g-4">
+          <Row className="row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-xl-6 g-4">
             {characters.map(character =>
               <Col className="d-flex" key={character.id}>
                 <StyledCard onClick={() => {
